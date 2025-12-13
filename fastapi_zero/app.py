fb4de0a4d58581d1a1139a7ec7fa55c13a22ba74
Francisco Edson Lopes da Silva <fco.edson.lopes@gmail.com>
2025-12-13 09:22:40 -0300

    Adiciona schemas.py

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -1,8 +1,10 @@
+from http import HTTPStatus
 from fastapi import FastAPI
+from fastapi_zero.schemas import Message
 
-app = FastAPI()
+app = FastAPI(title='FastAPI Zero', version='0.1.0')
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá, mundo'}
