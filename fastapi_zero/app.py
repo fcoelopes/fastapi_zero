002b2463b08c451f8d04f32c270967d84ceaa129
Francisco Edson Lopes da Silva <fco.edson.lopes@gmail.com>
2025-12-14 10:58:18 -0300

    Implementa rota /users/

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -2,7 +2,9 @@ from http import HTTPStatus
 
 from fastapi import FastAPI
 
-from fastapi_zero.schemas import Message
+from fastapi_zero.schemas import Message, UserDB, UserPublic, UserSchema
+
+database = []
 
 app = FastAPI(title='FastAPI Zero', version='0.1.0')
 
@@ -10,3 +12,10 @@ app = FastAPI(title='FastAPI Zero', version='0.1.0')
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá, mundo'}
+
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
+    database.append(user_with_id)
+    return user_with_id
--- tests/test_app.py
+++ tests/test_app.py
@@ -11,3 +11,21 @@ def test_root_deve_retornar_ola_mundo():
     # assert response.status_code == 200
     assert response.json() == {'message': 'Olá, mundo'}
     assert response.status_code == HTTPStatus.OK
+
+
+def test_create_user_deve_criar_e_retornar_usuario():
+    client = TestClient(app)
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'fcoed',
+            'email': 'fcoed1@example.com',
+            'password': 'strongpassword123',
+        },
+    )
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'username': 'fcoed',
+        'email': 'fcoed1@example.com',
+        'id': 1,
+    }
